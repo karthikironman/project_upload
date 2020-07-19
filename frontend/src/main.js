f78c028c70aa692b02876188bf92fe85981efa41
karthikironman <karthik.selvaraj@diatoz.com>
2020-07-19 11:32:01 +0530

    vue router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from "@/router/index.js";
 
 Vue.config.productionTip = false
 
 new Vue({
+  Router,
   render: h => h(App),
 }).$mount('#app')
