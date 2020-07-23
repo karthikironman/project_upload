d9522e3a319c3250a83ccd7ad523c7cd4ec009c0
karthik.selvaraj <karthik.selvaraj@diatoz.com>
2020-07-23 23:04:17 +0530

    sticky notes

--- frontend/src/components/pages/my_plan.vue
+++ frontend/src/components/pages/my_plan.vue
@@ -1,13 +1,130 @@
 <template>
   <div>
-    <w-navbar />my plan page
+    <w-navbar />
+    <div class="my-plan-page">
+      <sticky :item="items" v-for="(items,index) in notes" :key="index" @removeme="remove(index)" />
+      <div class="sticky-not" v-if="isempty()">ADD YOUR PLANS AS STICKY NOTES HERE</div>
+    </div>
+    <b-btn v-b-modal.modal-prevent-closing class="add-btn">
+      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+    </b-btn>
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="UPLOAD MY PLANS"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          :state="nameState"
+          label="Message"
+          label-for="name-input"
+          invalid-feedback="Message is required"
+        >
+          <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
+        </b-form-group>
+      </form>
+    </b-modal>
+    <div>
+      <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
   </div>
 </template>
 <script>
 import navb from "@/components/widgets/portalNav";
+import sticky from "@/components/widgets/stickyNotes.vue";
 export default {
   components: {
-    "w-navbar": navb
+    "w-navbar": navb,
+    sticky
+  },
+  data() {
+    return {
+      name: "",
+      nameState: null,
+      notes: [
+       
+      ]
+    };
+  },
+  methods:{
+    isempty(){
+     if(this.notes.length === 0)
+     return true;
+     else
+     return false;
+    },
+    remove(index){
+     console.log("removing",index);
+     this.notes.splice(index,1);
+    },
+    checkFormValidity() {
+        const valid = this.$refs.form.checkValidity()
+        this.nameState = valid
+        return valid
+      },
+      resetModal() {
+        this.name = ''
+        this.nameState = null
+      },
+      handleOk(bvModalEvt) {
+        // Prevent modal from closing
+        bvModalEvt.preventDefault()
+        // Trigger submit handler
+        this.handleSubmit()
+      },
+      handleSubmit() {
+        // Exit when the form isn't valid
+        if (!this.checkFormValidity()) {
+          return
+        }
+        // Push the name to submitted names
+        var notes = { "message":this.name, "createdDate":"UPLOAD STICKY NOTES"};
+        this.notes.push(notes)
+        // Hide the modal manually
+        this.$nextTick(() => {
+          this.$bvModal.hide('modal-prevent-closing')
+        })
+      }
+    
   }
 };
 </script>
+<style scoped>
+.my-plan-page {
+  background-color: #e7e2e2;
+  height: calc(100vh - 56px);
+  width: calc(100vw - 200px);
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  margin-left: 200px;
+  overflow-y:auto;
+}
+.add-btn {
+  position: absolute;
+  font-size: 35px;
+  border-radius: 50%;
+  background-color: #17a2b8;
+  height:4rem;
+  left:56.5%;
+  border-radius:9px;
+  top:90%;
+  transform: translate(-50%, -50%);
+}
+.sticky-not{
+  font-weight:900;
+  position:absolute;
+  left:50%;
+  top:50%;
+    border-radius:9px;
+  transform:translate(-50%,-50%);
+  background-color:yellow;
+  border:2px dotted grey;
+  padding:5rem;
+}
+</style>
---
+++ frontend/src/components/widgets/stickyNotes.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="sticky-notes" :style="{ transform: 'rotate('+ angle+'deg)'}">
+    <i class="fa fa-paperclip" aria-hidden="true"></i>
+    <p class="date">{{item.createdDate}}</p>
+    <p class="message">{{item.message}}</p>
+    <b-btn class="buttons" @click="removeme()">
+      <i class="fa fa-check" aria-hidden="true"></i>
+    </b-btn>
+  </div>
+</template>
+<script>
+export default {
+  props: ["item", "index"],
+  data() {
+    return {
+      angle: null,
+      color: null
+    };
+  },
+  created() {
+    this.angle = Math.floor(Math.random() * 20 - 10);
+  },
+
+  methods: {
+    removeme() {
+      this.$emit("removeme");
+    }
+  }
+};
+</script>
+<style scoped>
+.sticky-notes {
+  width: 200px;
+  height: 200px;
+  border: 1px solid white;
+  border-radius: 5px;
+  position: relative;
+  background-color: #f4f5b2;
+  float: left;
+  margin: 2rem;
+}
+.fa-paperclip {
+  position: absolute;
+  top: -1.3rem;
+  font-size: 50px;
+  left: 10rem;
+}
+.date {
+  font-style: arial;
+  font-size: 11px;
+  text-align: left;
+  padding-left: 0.8rem;
+}
+.message {
+  height: 67%;
+  width: 90%;
+  border: 1px solid grey;
+  overflow-y: auto;
+  margin: 0 auto;
+  text-align: left;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+.buttons {
+  background-color: #17a2b8;
+  border-radius: 50%;
+}
+.angle1 {
+  transform: rotate(-10deg);
+}
+.angle2 {
+  transform: rotate(-5deg);
+}
+.angle3 {
+  transform: rotate(0deg);
+}
+.angle4 {
+  transform: rotate(5deg);
+}
+.angle5 {
+  transform: rotate(10deg);
+}
+</style>
