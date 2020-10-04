47498b44ab2bb93d4891c161870dd87870d21276
karthik.selvaraj <karthik.selvaraj@diatoz.com>
2020-10-04 16:46:27 +0530

    api integration

--- frontend/src/components/pages/dashboard.vue
+++ frontend/src/components/pages/dashboard.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="dashboard">
     <w-navbar />
-    <div class="inner-body"><w-screen-widget v-for="i in 5" :key="i" /></div>
-    
+    <div class="inner-body">
+      <w-screen-widget
+        :device_id="i.device_id"
+        :url="i.url"
+        @update="refresh()"
+        v-for="(i,index) in item"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 <script>
+import axios from "axios";
 import navb from "@/components/widgets/portalNav";
-import screen from "@/components/widgets/screens.vue"
+import screen from "@/components/widgets/screens.vue";
 export default {
   components: {
     "w-navbar": navb,
-    "w-screen-widget":screen
+    "w-screen-widget": screen
+  },
+  data() {
+    return {
+      item: []
+    };
+  },
+  mounted() {
+    this.refresh();
+  },
+  methods: {
+    refresh() {
+      axios.get("http://karthik.buzz:1880/allscreens").then(res => {
+        this.item = res.data;
+      });
+    }
   }
 };
 </script>
 <style scoped>
-.inner-body{
-  margin-left:200px;
+.inner-body {
+  margin-left: 200px;
 }
 </style>
--- frontend/src/components/widgets/screens.vue
+++ frontend/src/components/widgets/screens.vue
@@ -4,18 +4,46 @@
       Device Id :
       <b>{{device_id}}</b>
     </p>
-    <img class="screen-img" :src="url" />
-    <b-button class="m-3 buttonn" variant="dark">UPLOAD</b-button>
+    <div v-if="image_name.length > 0" class="img-name">{{image_name}}</div>
+    <img v-else class="screen-img" :src="url" />
+    <input type="file" @change="onFileSelected" ref="fileInput" style="display:none" />
+    <button @click="$refs.fileInput.click()">CHANGE IMAGE</button>
+    <b-button @click="onUpload" class="m-3 buttonn" variant="dark">UPLOAD</b-button>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
+  props: ["device_id", "url"],
   data() {
     return {
-      device_id: "564756",
-      url:
-        "https://cache.careers360.mobi/media/presets/500X333/colleges/social-media/media-gallery/2330/2019/3/7/Events%20of%20Cambridge%20Institute%20of%20Technology%20Ranchi_Events.jpg"
+      image_name: "",
+      selectedFile: null
     };
+  },
+  methods: {
+    onFileSelected(event) {
+      console.log(event);
+      this.selectedFile = event.target.files[0];
+      this.image_name = this.selectedFile.name;
+    },
+    onUpload() {
+      const fd = new FormData();
+      fd.append("image", this.selectedFile, this.selectedFile.name);
+
+      axios
+        .post(
+          `http://karthik.buzz:1880/updateScreen?device_id=${this.device_id}`,
+          fd
+        )
+        .then(() => {
+          this.$emit("update");
+        })
+        .catch()
+        .finally(() => {
+          this.image_name = "";
+        });
+    }
   }
 };
 </script>
@@ -25,7 +53,7 @@ export default {
   width: 250px;
   border: 2px solid rgb(104, 94, 94);
   background-color: #d6d6f0;
-  margin: 1rem;
+  margin: 2rem;
   border-radius: 4px;
   float: left;
   -webkit-box-shadow: 0px -1px 30px -2px rgba(0, 0, 0, 0.75);
@@ -35,11 +63,21 @@ export default {
 .screen-img {
   width: 95%;
   border-radius: 4px;
+  max-height: 60%;
 }
 .device_id {
   font-size: 20px;
 }
-.buttonn{
-    border-radius:10px;
+.buttonn {
+  border-radius: 10px;
+}
+.img-name {
+  width: 95%;
+  overflow-wrap: break-word;
+  border-radius: 4px;
+  height: 9.9rem;
+  margin: 0 auto;
+  font-weight: 900;
+  padding-top: 4rem;
 }
 </style>
